bed5ee0e81b6b0e34d5da3d8d9623843dc58202d
Mario Cervantes <marioyeahs@gmail.com>
2021-10-01 03:59:42 -0500

    add: Index view show  Arduinos

--- quantumrobotics/arduinodata/views.py
+++ quantumrobotics/arduinodata/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Arduino
 # Create your views here.
+
+class IndexListView(ListView):
+    model=Arduino
+    template_name='./index.html'
+    def get_queryset(self):
+        return Arduino.objects.all()
+
