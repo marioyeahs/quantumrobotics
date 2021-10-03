8c50dc10183a15cdc5415f1f8f498ba78775cb5b
Mario Cervantes <marioyeahs@gmail.com>
2021-10-02 22:31:00 -0500

    addd:context to return arduino models

--- quantumrobotics/arduinodata/views.py
+++ quantumrobotics/arduinodata/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query_utils import Q
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Arduino
@@ -5,7 +6,21 @@ from .models import Arduino
 
 class IndexListView(ListView):
     model=Arduino
-    template_name='./index.html'
-    def get_queryset(self):
-        return Arduino.objects.all()
-
+    template_name='arduinodata/index.html'
+    def get_context_data(self,**kwargs):
+        context = super(IndexListView, self).get_context_data(**kwargs)
+        board=Arduino.BOARD
+        boards=[]
+        boards_key=[]
+        for i in range(len(board)):
+            boards_key+=[board[i][0]]
+            boards+=[board[i][1]]
+        
+        hard_by_model=[]
+        for i in boards_key:
+            hard_by_model+=[Arduino.objects.filter(board=i)]
+        
+        context['key_boards']=zip(boards_key,hard_by_model)
+        context['value_boards']=zip(boards_key,boards)
+        
+        return context
