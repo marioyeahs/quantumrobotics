98f5292d942ba47b009318671cda20c9d383b564
Mario Cervantes <marioyeahs@gmail.com>
2021-10-04 04:15:13 -0500

    add: url to show registers

--- quantumrobotics/arduinodata/urls.py
+++ quantumrobotics/arduinodata/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views 
 from .views import IndexListView
 
-app_name='quantumrobotics'
+app_name='arduinodata'
 urlpatterns = [
     path('',IndexListView.as_view(), name='index'),
-    path('model/<str:board>/', views.model, name='model')
+    path('model/<str:board>/', views.model, name='model'),
+    path('registers/<str:model_board>/', views.data, name='arduino-data')
 ]
