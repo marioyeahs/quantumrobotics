8c5413a1241a79e703f03155376c7f58fd354cac
Mario Cervantes <marioyeahs@gmail.com>
2021-10-04 04:14:36 -0500

    fix: model attributes

--- quantumrobotics/arduinodata/models.py
+++ quantumrobotics/arduinodata/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -12,11 +13,17 @@ class Arduino(models.Model):
     board = models.CharField(verbose_name="Board",max_length=3, choices=BOARD)
     board_name = models.CharField(max_length=255)
     def __str__(self):
-        return self.board_name
+        return f"{self.board} - {self.board_name}"
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('arduinodata:arduino-data', kwargs={'pk' : self.pk})
 
 class Data(models.Model):
     arduino = models.ForeignKey(Arduino, on_delete=models.CASCADE)
-    data = models.TextField(verbose_name="Data")
+    sensor = models.IntegerField(default='0')
+    content = models.TextField(verbose_name="Content",default='')
+    date = models.DateField(verbose_name="Date",default='')
     def __str__(self):
-        return f"{self.data} - Arduino:{self.arduino}"
+        return f"Sensor {self.sensor} - {self.arduino.board_name} - {self.date}"
+
 
